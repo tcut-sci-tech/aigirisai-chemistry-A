9a96a15f73ce1efb2d9462b299a2ebf8f7d209cb
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2025-09-27 02:44:48 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,91 @@
-// ページの読み込みが完了してからスクリプトを実行
-$(window).on('load', function () {
-
-  //テキストのカウントアップ+バーの設定
-  var bar = new ProgressBar.Line(splash_text, {//id名を指定
-    easing: 'easeInOut',//アニメーション効果
-    duration: 1000,//時間指定(1000＝1秒)
-    strokeWidth: 0.2,//進捗ゲージの太さ
-    color: '#555',//進捗ゲージのカラー
-    trailWidth: 0.2,//ゲージベースの線の太さ
-    trailColor: '#bbb',//ゲージベースの線のカラー
-    text: {
-      style: {
-        position: 'absolute',
-        left: '50%',
-        top: '50%',
-        padding: '0',
-        margin: '-30px 0 0 0',
-        transform: 'translate(-50%,-50%)',
-        'font-size': '1rem',
-        color: '#fff',
-      },
-      autoStyleContainer: false
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1000,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#555',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定       
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform:'translate(-50%,-50%)',
+      'font-size':'1rem',
+      color: '#fff',
     },
-    step: function (state, bar) {
-      bar.setText(Math.round(bar.value() * 100) + ' %');
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function(state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});  
+
+
+$(function(){
+  /*=================================================
+  ハンバーガ―メニュー
+  ===================================================*/
+  // ハンバーガーメニューをクリックした時
+  $('.hamburger').on('click', function() {
+    // ハンバーガーメニューの共通処理を呼び出す
+    hamburger();
+  });
+  // メニューのリンクをクリックした時
+  $('#navi a').on('click', function() {
+    // ハンバーガーメニューの共通処理を呼び出す
+    hamburger();
+  });
+
+  /*=================================================
+  Inview（画面に表示されたタイミングで処理を実行）
+  ===================================================*/
+  // BBBが選ばれる理由（スライド左）
+  $('.inview-slide-left').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
+      // 要素が表示されたらslide-leftクラスを追加
+      $(this).stop().addClass('slide-left');
     }
   });
-
-  // アニメーションスタート＋スプラッシュ非表示
-  bar.animate(1.0, function () {
-    $("#splash").delay(500).fadeOut(800);
+  // BBBが選ばれる理由（スライド右）
+  $('.inview-slide-right').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
+      // 要素が表示されたらslide-rightクラスを追加
+      $(this).stop().addClass('slide-right');
+    }
   });
-
+  // 受講生の声（ふきだし）
+  $('.inview-balloon').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
+      // 要素が表示されたらballoonクラスを追加
+      $(this).stop().addClass('balloon');
+    }
+  });
 });
+
+/*=================================================
+ハンバーガ―メニュー共通処理
+===================================================*/
+// ハンバーガーメニューをクリックした時とメニュー内のリンクをクリックした時の
+// 処理が同じなので処理を共通化する
+function hamburger() {
+  // toggleClassを使用することで、hamburgerクラスにactiveクラスが存在する場合は削除、
+  // 存在しない場合を追加する処理を自動で行ってくれる
+  $('.hamburger').toggleClass('active');
+
+  if ($('.hamburger').hasClass('active')) {
+    // hamburgerクラスにactiveクラスが存在する場合は、naviにもactiveクラスを追加する
+    $('#navi').addClass('active');
+  } else {
+    // hamburgerクラスにactiveクラスが存在しない場合は、naviからactiveクラスを削除する
+    $('#navi').removeClass('active');
+  }
+}
